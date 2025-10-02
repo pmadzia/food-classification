7d6f7ab4ad534e5a2d6a31d5cec433aaeb021c07
pmadzia <piotrmadzia22@gmail.com>
2025-10-02 16:49:03 +0200

    Added function for ploting loss and accuracy.

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,6 +1,7 @@
 """
-Contains utility functions for Pytorch model training, evaluating and saving.
+Contains utility functions for Pytorch model training, evaluating, ploting and saving.
 """
+import matplotlib.pyplot as plt
 import torch
 from pathlib import Path
 
@@ -45,3 +46,38 @@ def save_model(
     print(f'Saving model to: {model_save_path}.')
     torch.save(obj=model.state_dict(),
                f=model_save_path)
+
+def plot_loss_and_accuracy_curves(results: dict[str, list[float]]) -> None:
+    """Plots loss and accuracy curves based on results of model training.
+
+    Args:
+        results: A dictionary containing the results of training with the following keys.
+            - 'train_loss': List of training losses per epoch.
+            - 'test_loss': List of test losses per epoch.
+            - 'train_acc': List of training accuracies per epoch.
+            - 'test_acc': List of test accuracies per epoch.
+    """
+    loss = results['train_loss']
+    test_loss = results['test_loss']
+    accuracy = results['train_acc']
+    test_accuracy = results['test_acc']
+
+    epochs = range(len(results['train_loss']))
+
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='train_loss')
+    plt.plot(epochs, test_loss, label='test_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label='train_accuracy')
+    plt.plot(epochs, test_accuracy, label='test_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend()
